a6e694e49e0af325a91c05aea9b2ba55eaa3f929
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2021-04-12 08:59:16 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,26 +1,20 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import postRoutes from "./routes/posts.js";
+import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
 
-app.use("/posts", postRoutes);
-
-// Set View Engine
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("./index.ejs")
-})
+app.use('/posts', postRoutes);
 
 // const CONNECTION_URL = "mongodb://localhost/mern-project";
 const PORT = process.env.PORT || 5000;
@@ -34,4 +28,4 @@ mongoose
   )
   .catch((error) => console.log(error.message));
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
