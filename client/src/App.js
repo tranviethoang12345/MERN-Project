32ad1f8b93a990f9a22e1e598958e883afb85128
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2021-04-14 17:37:25 +0700

    back him 14-4

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { Container } from "@material-ui/core";
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Auth from './components/Auth/Auth';
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
   return (
